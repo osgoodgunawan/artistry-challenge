0d53aceeec72c2249b4273a508deef8afae1fbdb
Dwire <gjdwyer8@gmail.com>
2020-03-19 17:41:06 -0400

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,40 @@
 class ArtistsController < ApplicationController
+  def index 
+    @artists = Artist.all
+
+    # render :index
+  end 
+
+  def show 
+    @artist = Artist.find(params[:id])
+    @artist_instrument = ArtistInstrument.new
+    @instruments = Instrument.all
+    # render :show
+  end 
+
+  def new 
+    @artist = Artist.new
+
+    # render :new
+  end 
+
+  def create 
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      flash[:success] = "beutifull you have done and amazing job"
+      redirect_to @artist
+    else 
+      flash[:errors] = @artist.errors.full_messages
+      redirect_to new_artist_path
+    end 
+
+  end 
+
+
+  private 
+
+  def artist_params
+    params.require(:artist).permit(:name, :title, :age)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   
+  resources :artist_instruments, only: [:new, :create]
+  resources :artists, only: [:show, :index, :new, :create]
+
+  # get "/artist_instruments/new", to: 'artist_instruments#new', as: "new_artist_instrument"
+  # post "/artist_instruments", to: 'artist_instruments#create', as: "artist_instrument"
 end
